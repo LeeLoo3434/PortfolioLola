b88b87854add1a9501f6d6a7aab14f11a4ee3ca7
LeeLoo3434 <lolarussell347@gmail.com>
2023-05-20 12:11:50 -0700

    projectpage and links on dashboard page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import ProfileCarousel from '../components/ProfileCarousel';
 import '../styles.css';
 import '../components/ProfileCarousel.module.css';
@@ -7,6 +8,20 @@ export default function Dashboard() {
   return (
     <div>
       <ProfileCarousel />
+      <div className="flex justify-center mt-8">
+        <Link
+          to="/projects"
+          className="text-4xl font-bold inline-block animate-rainbow mr-6"
+        >
+          Projects
+        </Link>
+        <Link
+          to="/art-community"
+          className="text-4xl font-bold inline-block animate-rainbow"
+        >
+          Art + Community
+        </Link>
+      </div>
     </div>
   );
 }
