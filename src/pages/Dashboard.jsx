e537ff9b24c63f6146ce81f276bb20a6aff123b8
LeeLoo3434 <lolarussell347@gmail.com>
2023-06-18 08:38:34 -0700

    short and long bio

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,14 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import ProfileCarousel from '../components/ProfileCarousel';
 import Bio from '../components/Bio';
 import { ThemeContext } from '../ThemeContext';
 import '../styles.css';
-import styles from "./Dashboard.module.css";
 
 export default function Dashboard() {
   const { isDarkMode } = useContext(ThemeContext);
+  const containerClassName = isDarkMode ? 'container dark' : 'container light';
+  const bioClassName = isDarkMode ? 'textDark' : 'textLight';
+
   const [showShortBio, setShowShortBio] = useState(true);
 
   const handleShortBioClick = () => {
@@ -17,30 +19,17 @@ export default function Dashboard() {
     setShowShortBio(false);
   };
 
-  const containerClassName = isDarkMode ? 'container dark' : 'container light';
-  const bioClassName = isDarkMode ? 'textDark' : 'textLight';
-
   return (
     <div className={containerClassName}>
-      <ProfileCarousel />
-      <div className={`flex justify-center mt-8 ${styles.buttonContainer}`}>
-        <div className={`flex ${styles.buttonGroup}`}>
-          <button
-            onClick={handleShortBioClick}
-            className={`${styles.button} rounded-full py-2 px-4 ${showShortBio ? styles.active : ''}`}
-          >
-            Short
-          </button>
-          <button
-            onClick={handleLongBioClick}
-            className={`${styles.button} rounded-full py-2 px-4 ${!showShortBio ? styles.active : ''}`}
-          >
-            Long
-          </button>
-        </div>
+      <div className="flex justify-center mt-4">
+        <ProfileCarousel className="w-3/4" />
       </div>
       <div className={`flex justify-center mt-4 ${bioClassName}`}>
-        <Bio showShortBio={showShortBio} />
+        <Bio
+          showShortBio={showShortBio}
+          handleShortBioClick={handleShortBioClick}
+          handleLongBioClick={handleLongBioClick}
+        />
       </div>
     </div>
   );
